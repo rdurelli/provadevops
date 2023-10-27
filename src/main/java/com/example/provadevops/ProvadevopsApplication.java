3dfc051f265309d18ca3384ba559878719382897
Rafael Durelli <rafael.durelli@ufla.br>
2023-10-27 05:42:05 -0300

    feat: first commit

---
+++ src/main/java/com/example/provadevops/ProvadevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.provadevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvadevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvadevopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/provadevops/ProvadevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provadevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvadevopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
